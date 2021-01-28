75a3a028320e095adf2dc94302e813042694fdab
Stefan <info@bitcoinprivacy.net>
2021-01-28 10:35:48 +0100

    trying the new build

---
+++ new/service-worker.js
@@ -0,0 +1,31 @@
+importScripts("precache-manifest.870d42406272213b6d5be017dabe759a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.gstatic\.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: "google-fonts-webfonts",
+    plugins: []
+  }),
+  "GET"
+);
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "google-fonts-stylesheets",
+    plugins: []
+  }),
+  "GET"
+);
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "images",
+    plugins: []
+  }),
+  "GET"
+);
+
