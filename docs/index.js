d1d58be94ba7b294ca152cd91e87a108786d9d06
stefanwouldgo <stefan@sblbs.de>
2021-02-08 15:14:23 +0100

    trying to fix Cannot read property 'subscribe'

--- docs/index.js
+++ docs/index.js
@@ -2,8 +2,6 @@ export default function (elmLoaded) {
   console.log("Hello outside of promise!");
   elmLoaded.then((elmPagesApp) => {
     console.log("Inside of promise");
-    elmPagesApp.ports.example.subscribe((message) => {
-      console.log("Elm port message: ", message);
     });
   });
 }
